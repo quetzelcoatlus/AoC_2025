fd74b64bfcff329372e876fa92e0e6b500db1ab6
Quetzelcoatlus <jan.brzezanski@gmail.com>
2025-12-04 17:08:45 +0100

    Day 4
    In part 2 cool thing is that we don't have to remove rolls only after
    calculating how many can we remove (like shown visually on task) - we
    can just remove them as we go once found removable one

---
+++ 04/04b.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 256
+char tab[MAX_SIZE][MAX_SIZE];
+int w,h=1;
+
+int remove_rolls(){
+    int rolls=0;
+    for(int i=1; i<h; i++){
+        for(int j=1; j<=w; j++){
+            if(tab[i][j]){
+                int count=0;
+                for(int a=-1; a<=1; a++)
+                    for(int b=-1; b<=1; b++)
+                        if(a || b)
+                            count += tab[i+a][j+b];
+
+                if(count < 4){
+                    rolls++;
+                    tab[i][j]=0;
+                }
+            }
+        }
+    }
+
+    return rolls;
+}
+
+int main(int argc, char* argv[]){
+     FILE* f = fopen(argc > 1 ? argv[1] : "input", "r");
+     int sum=0, rolls;
+
+     while(fscanf(f,"%s\n", &tab[h][1]) == 1){
+         w=strlen(&tab[h][1]);
+         for(int i=1; i<=w; i++)
+            tab[h][i] = tab[h][i] == '@' ? 1 : 0;
+
+         h++;
+     }
+
+     while((rolls = remove_rolls()) > 0) sum+=rolls;
+
+     printf("%d\n",sum);
+
+     return 0;
+}
